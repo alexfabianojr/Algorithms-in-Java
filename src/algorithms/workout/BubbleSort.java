9ca86ded256d5a56bffb1dbe7286de5fc1b671b4
alexfabianojr <alexfabianojr@gmail.com>
2021-07-04 14:33:26 -0300

    variavel temporaria do bubble sort movida para fora do for

--- src/algorithms/workout/BubbleSort.java
+++ src/algorithms/workout/BubbleSort.java
@@ -1,21 +1,22 @@
 package algorithms.workout;
 
+/**
+ * {3, 4, 5}
+ * pego o 3 na posição inicial e procuro alguém maior que ele a partir da posição inicial + 1
+ * Caso encontre eu armazeno o 3 na variavel temporaria, coloco o maior na posição do 3
+ * e o 3 na posição antiga do outro (j) e continua o loop [...]
+ * */
+
 public class BubbleSort {
     public static void main(String[] args) {
-        var valores = new int[]{21, 11, 44, 2, 12, 2, 9, 10, 15, 1, 100};
+        var valores = new int[]{21, 11, 44, 2, 12, 2, 9, 10, 15, 1, 100, -2};
         asc(valores);
         desc(valores);
     }
 
     private static void desc(int[] valores) {
-        /**
-         * {3, 4, 5}
-         * pego o 3 na posição inicial e procuro alguém maior que ele a partir da posição inicial + 1
-         * Caso encontre eu armazeno o 3 na variavel temporaria, coloco o maior na posição do 3
-         * e o 3 na posição antiga do outro (j) e continua o loop [...]
-         * */
+        var temp = 0;
         for (int i = 0; i < valores.length; i++) {
-            var temp = 0;
             for (int j = i + 1; j < valores.length; j++) {
                 if (valores[j] > valores[i]) {
                     temp = valores[i];
@@ -36,8 +37,8 @@ public class BubbleSort {
     }
 
     private static void asc(int[] valores) {
+        var temp = 0;
         for (int i = 0; i < valores.length; i++) {
-            var temp = 0;
             for (int j = i + 0; j < valores.length; j++) {
                 if (valores[j] < valores[i]) {
                     temp = valores[i];
